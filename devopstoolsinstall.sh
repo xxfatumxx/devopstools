2e3575c52e38dc0e605330dcac0d35a47fc6bd02
xxfatumxx <xxfatumxx@gmail.com>
2019-06-07 16:39:42 +0300

    Hardcoded xenial repo for docker

--- devopstoolsinstall.sh
+++ devopstoolsinstall.sh
@@ -25,7 +25,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) \
+xenial \
 stable"
 
 # Update repositories
