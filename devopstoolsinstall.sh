7f404a38bf118344f4f23d41ea23c1acf77bbb54
xxfatumxx <xxfatumxx@gmail.com>
2019-06-07 13:00:02 +0300

    Linux script. Trying to use snap more.

--- devopstoolsinstall.sh
+++ devopstoolsinstall.sh
@@ -9,38 +9,83 @@ apt update && apt upgrade -y
 
 ## 2. Add all neccesary apt repositories
 
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
 ## 3. Install apps & tools
 apt install \
-7zip \
-brave \
-dbeaver \
-dropbox \
-f.lux \
+# p7zip-full \
+# dbeaver \
+# dropbox ?
+# f.lux \
 firefox \
-gcloudsdk \
+# gcloudsdk \
 git \
-GoogleChrome \
-openjdk8 \
+google-chrome-stable \
+openjdk-8-jre \
 keepass2 \
-kubectl \ 
-helm \
-lightscreen \
-xneur \
-minikube \
-notepadplusplus \
-openvpn \
-postman \
-yaquake \
+# kubectl \ 
+# helm \
+gnome-screenshot \
+remmina \
+gxneur \
+# minikube \
+# notepadplusplus \
+# openvpn \
+# postman \
+guake \
 transmission \
-skype \
-slack \
-teamviewer \
+# skype \
+# slack \
+# teamviewer \
 vagrant \
 virtualbox \
 vlc \
-vscode \
+# vscode \
 mtr \
 mc \
--y 
+-y
+
+## 4. Snap installation
+
+SNAPS=" 
+brave
+p7zip-desktop
+minikube
+notepad-plus-plus
+postman
+"
+
+for SNAP in ${SNAPS} do
+snap install $SNAP
+done
+
+SNAPS-CLASSIC="
+kubectl
+helm
+google-cloud-sdk
+skype
+slack
+code
+"
+
+for SNAP in ${SNAPS-CLASSIC} do
+snap install $SNAP --classic
+done
+
+SNAPS-EDGE="
+dbeaver-ce
+fluxgui
+"
+
+for SNAP in ${SNAPS-EDGE} do
+snap install $SNAP --edge
+done
+
+## 5. Manual installation
+
+# Team Viewer
+deb -i https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
 ### End ###
