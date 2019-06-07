5f41abc5e4f99631285cdf82ab6e0abc936005b2
xxfatumxx <xxfatumxx@gmail.com>
2019-06-07 14:11:16 +0300

    Update apt repos

--- devopstoolsinstall.sh
+++ devopstoolsinstall.sh
@@ -13,6 +13,9 @@ apt update && apt upgrade -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
+# Update repositories
+apt update
+
 ## 3. Install apps & tools
 apt install \
 firefox \
